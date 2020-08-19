890dad5db4a3718deca00b75dca02177fa9bd61e
Francis <francis.munabeno@yahoo.fr>
2020-08-19 15:49:56 +0200

    config api and mongo js

---
+++ api/api.js
@@ -0,0 +1,29 @@
+const db = require('../libs/mongo')
+const express = require('express')
+
+let app = express()
+
+// DB MONGO
+const mongo = db.connectBdd()
+
+// PORT
+const port = 8080
+
+app.get('/availableStudents', async (req,res, next) => {
+    const result = await available(next)
+    res.json(result)
+})
+
+available = async ( next) => {
+  try{
+    const db = await mongo
+    const result = db.collection('Available_Students').find().toArray()
+    return result
+  }catch (err){
+    next(err)
+  } finally{
+    db.close()
+  }
+}
+ 
+app.listen(port)
---
+++ libs/mongo.js
@@ -0,0 +1,25 @@
+var MongoClient = require('mongodb').MongoClient
+
+var client;
+var MONGO_DBNAME = "tech-watch"
+var MONGO_URL = "mongodb://localhost:27017"
+
+connectBdd = async() => {
+  try{
+    if(!client) client = await MongoClient.connect(MONGO_URL, { useUnifiedTopology: true })
+    return {
+      db: client.db(MONGO_DBNAME),
+      client: client
+    }
+  }catch(err){
+    console.error(" ERROR MONGO => ", err)
+  }
+}
+
+close = () => {
+  if(client) client.close()
+  client = undefined
+}
+
+exports.connectBdd = connectBdd
+exports.close = close
